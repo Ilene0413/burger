772bae27039e6d15125c2f2a150e29a82e02e6ef
Ilene Cohen <ilene413@icloud.com>
2019-02-20 15:21:14 -0500

    first version

---
+++ server.js
@@ -0,0 +1,30 @@
+let express = require("express");
+
+let app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+let PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
